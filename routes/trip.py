42b25d5438c5d33aa0cc3ffeca0f04bd78ee3a6f
Sakai <135956101+Sakai1123@users.noreply.github.com>
2025-01-16 14:35:43 +0900

    検索機能の修正と表示しないときの動作

--- routes/trip.py
+++ routes/trip.py
@@ -6,23 +6,22 @@ from peewee import fn
 # Blueprintの作成
 trip_bp = Blueprint("trip", __name__, url_prefix="/trip")
 
-
-@trip_bp.route("/", methods=['GET', 'POST'])
+@trip_bp.route('/', methods=['GET', 'POST'])
 def list():
 
-    if request.method == 'POST':
-        address = request.form.get('address', '').strip()
+    # addressがクエリパラメータで指定されているかを確認
+    address = request.args.get("address")
 
+    if address:  # addressが指定されている場合
         trip_data = Place.select()\
-            .where(Place.evaluation == 5,Place.address.contains(address))\
+            .where(Place.evaluation == 5, Place.address.contains(address))\
             .order_by(Place.day.desc())\
             .limit(5)
         
-        return render_template("trip.html",trip_data = trip_data)
-        
-    trip_data = Place.select()\
-        .where(Place.evaluation == 5)\
-        .order_by(Place.day.desc())\
-        .limit(5)
+    else:  # 初回ロードまたはaddressが指定されていない場合
+        trip_data = Place.select()\
+            .where(Place.evaluation == 5)\
+            .order_by(Place.day.desc())\
+            .limit(5)
 
-    return render_template("trip.html",trip_data = trip_data)
+    return render_template("trip.html", trip_data = trip_data, address = address)
